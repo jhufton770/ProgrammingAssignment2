5f7f147d92db7659ee08eea6600681ebade5bdcc
Jim Hufton <jhufton@yahoo.com>
2014-10-21 11:06:05 -0500

    Updated Comments File
    Moved the local location of the repository, needed to check that it can
    still sync to GitHub.

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinvert <- function(solve) m <<- solve
+    getinvert <- function() m
+    list(set = set, get = get,
+         setinvert = setinvert,
+         getinvert = getinvert)
+    
 
 }
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinvert()
+    if (!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setinvert(m)
+    m
 }
